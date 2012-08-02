40a92ede8d9b2624a0faa2a53adbc02645c7225b
Kyle Gann <kylegann@gmail.com>
2012-08-02 13:08:13 -0400

    added speclj for testing

--- project.clj
+++ project.clj
@@ -1,6 +1,15 @@
 (defproject clojhare "0.0.1"
   :description "Consume rabbitmq messages"
   :url "http://github.com/kgann/clojhare"
-  :dependencies [[org.clojure/clojure "1.3.0"] [korma "0.3.0-beta7"] [com.mefesto/wabbitmq "0.2.2"]]
-  :plugins [[lein-ring "0.4.5"]]
+  :dependencies [
+    [org.clojure/clojure "1.4.0"]
+    [korma "0.3.0-beta7"]
+    [com.mefesto/wabbitmq "0.2.2"]
+    [speclj "2.1.2"]
+  ]
+  :plugins [
+    [lein-ring "0.4.5"]
+    [speclj "2.1.2"]
+  ]
+  :test-paths ["spec/"]
   :main clojhare.core)
