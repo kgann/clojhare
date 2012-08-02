317cca1eb2fd63f4ed45f5e60a31cdf499e874fb
Kyle Gann <kylegann@gmail.com>
2012-08-02 13:15:15 -0400

    changed project.clj formatting

--- project.clj
+++ project.clj
@@ -5,11 +5,9 @@
     [org.clojure/clojure "1.4.0"]
     [korma "0.3.0-beta7"]
     [com.mefesto/wabbitmq "0.2.2"]
-    [speclj "2.1.2"]
-  ]
+    [speclj "2.1.2"]]
   :plugins [
     [lein-ring "0.4.5"]
-    [speclj "2.1.2"]
-  ]
+    [speclj "2.1.2"]]
   :test-paths ["test/spec/"]
   :main clojhare.core)
