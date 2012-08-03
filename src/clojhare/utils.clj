23e12249414cf495f5a650a889b85f4cc6a5ad6e
Kyle Gann <kylegann@gmail.com>
2012-08-03 13:44:19 -0400

    syntax changes

--- src/clojhare/utils.clj
+++ src/clojhare/utils.clj
@@ -13,15 +13,17 @@
 (defn consume-with
   "Expects a function and a queue name to be provided
    - Optional host address -  defaults to local rabbit
-   - ack? boolean - f must return true to ack
-   Lazely consumes messages and executes f(msg)"
-  ([f queue-name] (consume-with f queue-name "amqp://localhost"))
-  ([f queue-name host] (consume-with f queue-name host true))
-  ([f queue-name host ack?]
+   - ack? boolean - func must return true to ack
+   Lazely consumes messages and executes func(msg)"
+  ([func queue-name]
+    (consume-with func queue-name "amqp://localhost"))
+  ([func queue-name host]
+    (consume-with func queue-name host true))
+  ([func queue-name host ack?]
     (amqp/with-broker {:uri host}
       (amqp/with-channel
         (amqp/with-queue queue-name
           (doseq [msg (amqp/consuming-seq)]
-            (let [result (f (body msg))]
+            (let [result (func (body msg))]
               (if (and result ack?)
                 (ack msg)))))))))
